62ad9f8def1b872d6d9619ad93d96854bd981c8a
kash0k <ashok.kollipara@ibexlabs.com>
2024-03-19 23:19:10 +0530

    finish 01 and 02 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, count) {
+    let outputString = '';
+    if (count == 0 || phrase == '') {
+        return ""
+    } else if ( count < 0 ) {
+        return "ERROR"
+    } 
+    for (let i=1; i<=count; i++) {
+        outputString+=phrase;
+    }
+    return outputString
 };
 
 // Do not edit below this line
