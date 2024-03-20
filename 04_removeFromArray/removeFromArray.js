20f441dae9dda833371b8d18b106d7f22e3f8e47
kash0k <ashok.kollipara@ibexlabs.com>
2024-03-20 21:58:52 +0530

    finish 03 and 04 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(baseArray, ...args) {
+    let outputArray = [];
+    for (item of baseArray) {
+        if (!args.includes(item)) {
+            outputArray.push(item);
+        }
+    }
+    return outputArray
 };
 
 // Do not edit below this line
