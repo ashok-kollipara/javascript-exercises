042b03634903dcfda3fdc7a2fa7e22bed642f761
kash0k <ashok.kollipara@ibexlabs.com>
2024-03-20 22:44:13 +0530

    finish 05 and 06 tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    if (inputYear % 4 === 0) {
+        if (inputYear % 100 === 0) {
+            if (inputYear % 400 === 0) {
+                return true
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
