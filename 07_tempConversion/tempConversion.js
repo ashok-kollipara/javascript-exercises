aa8c46bcd2510b46740185c0e12f58fdec1a908c
kash0k <ashok.kollipara@ibexlabs.com>
2024-03-21 09:52:24 +0530

    finish 07 and 08 tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degF) {
+  return +((5 * (degF - 32)) / 9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degC) {
+  return +((9 * degC) / 5 + 32).toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  let total = 0;
+  for (num of numArray) {
+    total += num;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let total = 0;
+  if (numArray.length > 0) {
+    total=1;
+  }
+  for (num of numArray) {
+    total *= num;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+  return num ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  for (i=num; i>=1; i--) {
+    total *= i;
+  }
+  return total;
 };
+factorial(5)
 
 // Do not edit below this line
 module.exports = {
