422b59ce3a532ab566e586843a5180f0a8ad1d0c
kash0k <ashok.kollipara@ibexlabs.com>
2024-03-30 11:39:27 +0530

    solve 09 and 10 tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    inputString = inputString.toLowerCase();
+    let input = inputString.matchAll("[a-zA-Z0-9]+");
+    input = Array.from(input);
+    input = input.flat();
+    input = input.join("");
+    let revArr = input.split("");
+    revArr.reverse();
+    revArr = revArr.join("");
+    return ( revArr  === input )
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    pos = parseInt(pos);
+    let init = [0,1];
+    if (pos < 0) {
+        return "OOPS"
+    }
+    for (let i=2; i<=pos; i++) {
+        init[i] = init[i-1] + init [i-2];
+    }
+    return init[pos];
 };
 
 // Do not edit below this line
