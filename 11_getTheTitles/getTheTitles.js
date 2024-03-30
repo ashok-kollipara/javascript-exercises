eb8fe27020c9670586f39fa37c5e6b6942908e55
kash0k <ashok.kollipara@ibexlabs.com>
2024-03-30 14:33:11 +0530

    solve 11 and 12 tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookObjArr) {
+    const returnArr = [];
+    for (obj of bookObjArr) {
+        returnArr.push(obj.title);
+    }
+    return returnArr;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
+    let oldest = persons[0];
+    let age = (yearOfDeath, yearOfBirth) => {
+        if (yearOfDeath == undefined) {
+            yearOfDeath = new Date().getFullYear();
+        }
+        return yearOfDeath - yearOfBirth;
+    }
+    persons.map((person) => {
+        if (age(person.yearOfDeath, person.yearOfBirth) > age(oldest.yearOfDeath, oldest.yearOfBirth)) {
+            oldest = person;
+        }
+    });
 
+    return oldest;
 };
 
 // Do not edit below this line
